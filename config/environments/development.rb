0082816e34fe3b69bf4d40566e4fcc2fb0fa590e
dmytrozaremba <dmytrozaremba@gmail.com>
2021-05-22 00:20:40 +0300

    Deploy to heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  config.hosts << 'polar-fortress-66422.herokuapp.com'
+
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
